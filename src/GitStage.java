9af63908c03dafc5caf80f602303dc28d4c6b17f
Resul Acar <acar-resul-38@hotmail.com>
2023-07-29 18:42:17 +0300

    20230729.v1.Racar

---
+++ src/GitStage.java
@@ -0,0 +1,11 @@
+public class GitStage {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+
+        // git add contect root path ile sadece contect root adresi girilen dosyayi gönderir
+        // git add. tüm dosyalari stage area atar
+
+
+    }
+}
